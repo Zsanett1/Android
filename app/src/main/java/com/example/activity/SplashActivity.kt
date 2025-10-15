553f2150edace0ba916ccb80d4f086abb9b162b5
Raduly Zsanett <126141775+Zsanett1@users.noreply.github.com>
2025-10-15 09:41:16 +0300

    Lab1 folytatasa

--- app/src/main/java/com/example/activity/SplashActivity.kt
+++ app/src/main/java/com/example/activity/SplashActivity.kt
@@ -1,11 +1,44 @@
 package com.example.activity
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Log.d(TAG, "onCreate: SplashActivity created.")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: SplashActivity started.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: SplashActivity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: SplashActivity paused.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: SplashActivity stopped.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: SplashActivity restarted.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: SplashActivity destroyed.")
     }
 }
