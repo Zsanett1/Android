9308a2a2a946d0da4c74701b34902f47cdad5be0
Raduly Zsanett <126141775+Zsanett1@users.noreply.github.com>
2025-10-29 09:32:44 +0200

    Lab1(vagyis labor4) befejezve

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -4,12 +4,22 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import com.example.activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: MainActivity created.")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val message = intent.getStringExtra("message")
+        binding.textViewDisplay.text = message
     }
 
     override fun onStart() {
--- app/src/main/java/com/example/activity/SplashActivity.kt
+++ app/src/main/java/com/example/activity/SplashActivity.kt
@@ -1,15 +1,31 @@
 package com.example.activity
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.example.activity.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Log.d(TAG, "onCreate: SplashActivity created.")
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener {
+            val message = binding.editTextInput.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("message", message)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
